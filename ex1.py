01951dbdc50bf05bf66fbf0845a2ff11a0b4e115
Matheus Eckel Kleinschmidt <matheus.kleinschmidt@catolicasc.edu.br>
2023-08-29 21:36:23 -0300

    Add readme with user histories

--- ex1.py
+++ ex1.py
@@ -17,12 +17,27 @@ def moveTrain(commands, initialPosition=0, begin=-math.inf, end=math.inf):
         if (currentPosition > begin and currentPosition < end):
             currentPosition += command.value
 
-    return currentPosition
+    return "Resultado: " + str(currentPosition)
 
 
 print(moveTrain(commands=[Directions.ESQUERDA,
-      Directions.ESQUERDA], initialPosition=0))
+      Directions.ESQUERDA, Directions.ESQUERDA, Directions.ESQUERDA, Directions.ESQUERDA], initialPosition=0))
 print(moveTrain(commands=[Directions.ESQUERDA,
-      Directions.DIREITA], initialPosition=1, begin=-2))
-print(moveTrain(commands=[Directions.DIREITA,
-      Directions.ESQUERDA], initialPosition=1, begin=-2, end=10))
+      Directions.ESQUERDA, Directions.ESQUERDA, Directions.ESQUERDA, Directions.ESQUERDA], initialPosition=1, begin=-2))
+
+commands = []
+i = 0
+while i < 30:
+    i += 1
+    result = input("Direção " + str(i) + ": ")
+    if (result == "DIREITA"):
+        commands.append(Directions.DIREITA)
+    elif (result == "ESQUERDA"):
+        commands.append(Directions.ESQUERDA)
+    elif (result == "FIM"):
+        break
+
+initialPosition = int(input("Posição inicial: "))
+begin = int(input("Começo: "))
+end = int(input("Fim: "))
+print(moveTrain(commands, initialPosition, begin, end))
---
+++ readme.md
@@ -0,0 +1,13 @@
+História de usuários 
+
+EX1
+
+**Como** gerente de uma estação de trem, **Eu quero** um sistema que eu passe uma série de comandos **Para que** O trem autônomo siga 
+
+EX2
+
+**Como** gerente de uma estação de trem, **Eu quero** que o sistema agora tenha um trilho finito, e ele não deve extrapolar esse limite, **Para que** eu posso administrar melhor esse trem 
+
+EX3
+
+**Como** gerente de uma estação de trem, **eu quero** um sistema que pergunte aos usuários as direções do trem, a posição inicial do trem e os limites inferior e superior dos trilhos, **Para que** eu posso administrar melhor esse trem autônomo
